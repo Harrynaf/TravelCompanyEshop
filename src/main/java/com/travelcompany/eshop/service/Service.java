04dba149fe6f08132eba547b2f26b88f98ee39c7
Harry Nafpaktitis <hnafpaktitis@gmail.com>
2022-03-03 16:23:18 +0200

    Final_UpdatedProjectStracture_Changed_Lists

--- src/main/java/com/travelcompany/eshop/service/Service.java
+++ src/main/java/com/travelcompany/eshop/service/Service.java
@@ -4,15 +4,20 @@
  */
 package com.travelcompany.eshop.service;
 
-import com.travelcompany.eshop.model.BusinessCustomer;
 import com.travelcompany.eshop.model.Customer;
-import com.travelcompany.eshop.model.IndividualCustomer;
 import com.travelcompany.eshop.model.Itinerary;
 import com.travelcompany.eshop.model.ItineraryCityCodes;
 import com.travelcompany.eshop.model.Ticket;
+import com.travelcompany.eshop.repo.CustomerRepo;
+import com.travelcompany.eshop.repo.CustomerRepoImpl;
+import com.travelcompany.eshop.repo.ItineraryRepo;
+import com.travelcompany.eshop.repo.ItineraryRepoImpl;
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -25,69 +30,105 @@ public class Service {
     public static final String BLANK = "";
 
     public void runService() {
-
+        //Repo and service
+        ItineraryRepo itineraryRepoImpl = new ItineraryRepoImpl();
+        CustomerRepo customerRepoImpl = new CustomerRepoImpl();
+        TicketService ticketServiceImpl = new TicketServiceImpl();
+        CustomerService customerServiceImpl = new CustomerServiceImpl();
+        ItineraryService itineraryServiceImpl = new ItineraryServiceImpl();
         //Populate with new customers itineraries and tickets
         Instant instant = Instant.parse("2022-02-01T13:35:00.00Z");
         Instant instant1 = Instant.parse("2022-04-01T13:35:00.00Z");
-        Itinerary[] Itineraries = new Itinerary[SIZE];
-        Itineraries[0] = ItineraryService.CreateItinerary("1", ItineraryCityCodes.ATH, ItineraryCityCodes.BER, "SkyLines", Date.from(instant), new BigDecimal("120"));
-        Itineraries[1] = ItineraryService.CreateItinerary("2", ItineraryCityCodes.ATH, ItineraryCityCodes.PAR, "SkyLines", Date.from(instant1), new BigDecimal("140"));
-        Itineraries[2] = ItineraryService.CreateItinerary("3", ItineraryCityCodes.AUS, ItineraryCityCodes.NYC, "SkyLines", Date.from(instant), new BigDecimal("180"));
-        Itineraries[3] = ItineraryService.CreateItinerary("4", ItineraryCityCodes.NYC, ItineraryCityCodes.AUS, "SkyLines", Date.from(instant1), new BigDecimal("170"));
-        Itineraries[4] = ItineraryService.CreateItinerary("5", ItineraryCityCodes.CHN, ItineraryCityCodes.JPN, "SkyLines", Date.from(instant), new BigDecimal("190"));
-        Itineraries[5] = ItineraryService.CreateItinerary("6", ItineraryCityCodes.ATH, ItineraryCityCodes.NYC, "SkyLines", Date.from(instant1), new BigDecimal("195"));
-        Customer[] Customers = new Customer[SIZE];
-        Customers[1] = new BusinessCustomer("A1", "Harry", "Naf", "asdas@dasd.com", "asda", "Greek");
-        Customers[2] = new BusinessCustomer("A2", "1", "1", "1asdas@dasd.com", "1asda", "Greek");
-        Customers[3] = new IndividualCustomer("B3", "2", "2", "21asdas@dasd.com", "21asda", "Chinese");
-        Customers[4] = new IndividualCustomer("G3", "2", "2", "21asdas@dasd.com", "21asda", "Japanese");
-        Ticket[] Tickets = new Ticket[SIZE];
-        int ticketBoughtCount = 3;
-        Tickets[0] = TicketService.BuyTicket(Itineraries[1], Customers[1], "cash");
-        Tickets[1] = TicketService.BuyTicket(Itineraries[2], Customers[2], "cash");
-        Tickets[2] = TicketService.BuyTicket(Itineraries[3], Customers[3], "card");
-
+        List<Itinerary> Itineraries = new ArrayList<>();
+        //Itinerary[] Itineraries = new Itinerary[SIZE];
+        Itineraries.add(itineraryRepoImpl.CreateItinerary("1", ItineraryCityCodes.ATH, ItineraryCityCodes.BER, "SkyLines", Date.from(instant), new BigDecimal("120")));
+        Itineraries.add(itineraryRepoImpl.CreateItinerary("2", ItineraryCityCodes.ATH, ItineraryCityCodes.PAR, "SkyLines", Date.from(instant1), new BigDecimal("140")));
+        Itineraries.add(itineraryRepoImpl.CreateItinerary("3", ItineraryCityCodes.AUS, ItineraryCityCodes.NYC, "SkyLines", Date.from(instant), new BigDecimal("180")));
+        Itineraries.add(itineraryRepoImpl.CreateItinerary("4", ItineraryCityCodes.NYC, ItineraryCityCodes.AUS, "SkyLines", Date.from(instant1), new BigDecimal("170")));
+        Itineraries.add(itineraryRepoImpl.CreateItinerary("5", ItineraryCityCodes.CHN, ItineraryCityCodes.JPN, "SkyLines", Date.from(instant), new BigDecimal("190")));
+        Itineraries.add(itineraryRepoImpl.CreateItinerary("6", ItineraryCityCodes.ATH, ItineraryCityCodes.NYC, "SkyLines", Date.from(instant1), new BigDecimal("195")));
+        // Customer[] Customers = new Customer[SIZE];
+        List<Customer> Customers = new ArrayList<>();
+        Customers.add(customerRepoImpl.CreateBusinessCustomer("A1", "Harry", "Naf", "asdas@dasd.com", "asda", "Greek"));
+        Customers.add(customerRepoImpl.CreateBusinessCustomer("A2", "1", "1", "1asdas@dasd.com", "1asda", "Greek"));
+        Customers.add(customerRepoImpl.CreateIndividualCustomer("B3", "2", "2", "21asdas@dasd.com", "21asda", "Chinese"));
+        Customers.add(customerRepoImpl.CreateIndividualCustomer("G3", "2", "2", "21asdas@dasd.com", "21asda", "Japanese"));
+        List<Ticket> Tickets = new ArrayList<>();
+        //Ticket[] Tickets = new Ticket[SIZE];
+        //int ticketBoughtCount = 3;
+        Tickets.add(ticketServiceImpl.BuyTicket(Itineraries.get(0), Customers.get(0), "cash"));
+        Tickets.add(ticketServiceImpl.BuyTicket(Itineraries.get(1), Customers.get(1), "cash"));
+        Tickets.add(ticketServiceImpl.BuyTicket(Itineraries.get(2), Customers.get(2), "card"));
         //Main code
-        Customers[0] = CustomerService.CreateCustomer();
+        System.out.println("Please enter customer code,name,surname,email,address,nationality and category(Business/Individual), in that order");
+        Customer thisCustomer = customerServiceImpl.CreateCustomer();
+        Customers.add(thisCustomer);
+        System.out.println("Customer created");
         String option = BLANK;
         String option1;
         Itinerary chosenItinerary = null;
-        System.out.println("You are logged in with new user: credentials " + Customers[0]);
+        System.out.println("You are logged in with new user: credentials " + thisCustomer);
         while (!option.equals("exit")) {
             System.out.println("Enter a number corresponding to a following action: 1.Buy a ticket 2.List of the total number and cost of tickets for all customers 3.List of the total offered itineraries per destination and departure \n4.List of the customers who purchased the most tickets and the number of purchases 5.List of the customers who have not purchased any tickets, 'exit' to close");
             Scanner myObj = new Scanner(System.in);  // Create a Scanner object     
             option = myObj.nextLine();
             if (option.equals("1")) {
                 System.out.println("Choose one of the following itineraries by typing the code");
-                ItineraryService.showAllItineraries(Itineraries);
+                for (Itinerary Itinerarie : Itineraries) {
+                    System.out.println(Itinerarie);
+                }
                 option = myObj.nextLine();
-                for (int count = 0; count < Itineraries.length; count++) {
-                    if (Itineraries[count] != null) {
-                        if (Itineraries[count].getCode().equals(option)) {
-                            chosenItinerary = Itineraries[count];
+                for (Itinerary Itinerarie : Itineraries) {
+                    if (Itinerarie != null) {
+                        if (Itinerarie.getCode().equals(option)) {
+                            chosenItinerary = Itinerarie;
                         }
                     }
                 }
                 System.out.println("Would you like to buy buy cash or card");
                 option = myObj.nextLine();
-                Tickets[ticketBoughtCount] = TicketService.BuyTicket(chosenItinerary, Customers[0], option);
-                ticketBoughtCount++;
+                Ticket boughtTicket = ticketServiceImpl.BuyTicket(chosenItinerary, thisCustomer, option);
+                Tickets.add(boughtTicket);
+                System.out.println("Ticket purchase successful " + boughtTicket);
 
             }
             if (option.equals("2")) {
-                TicketService.showAllTickets(Tickets);
+                for (Ticket Tickett : Tickets) {
+                    if (Tickett != null) {
+                        System.out.println(Tickett);
+                    }
+                }
+                System.out.println(ticketServiceImpl.showTicketsTotalCost(Tickets));
+                System.out.println(ticketServiceImpl.showTicketsTotalNumber(Tickets));
+
             }
             if (option.equals("3")) {
                 System.out.println("Enter correctly the itinerary departure and destinaiton codes");
                 option = myObj.nextLine();
                 option1 = myObj.nextLine();
-                ItineraryService.showAllItinerariesPerDestDep(Itineraries, option, option1);
+                List<Itinerary> ChosenItineraries = itineraryServiceImpl.showAllItinerariesPerDestDep(Itineraries, option, option1);
+                for (Itinerary key : ChosenItineraries) {
+                    System.out.println(key);
+                }
             }
             if (option.equals("4")) {
-                CustomerService.showCustomersWithMostTickets(Customers, Tickets);
+                Map<Customer, Integer> CustomerWithTickets;
+                CustomerWithTickets = customerServiceImpl.showCustomersWithMostTickets(Customers, Tickets);
+                for (Customer key : CustomerWithTickets.keySet()) {
+
+                    System.out.println(key + " = " + CustomerWithTickets.get(key));
+
+                }
             }
             if (option.equals("5")) {
-                CustomerService.showCustomersWithoutTickets(Customers, Tickets);
+
+                Map<Customer, Integer> CustomerWithoutTickets = customerServiceImpl.showCustomersWithoutTickets(Customers, Tickets);;
+                for (Customer key : CustomerWithoutTickets.keySet()) {
+
+                    System.out.println(key + " = " + CustomerWithoutTickets.get(key));
+
+                }
+
             }
         }
     }
